11aac01384da2cc30b8f35fed5d618aa1852f44a
jush0147 <jush0147@gmail.com>
2023-08-07 18:08:44 +0800

    fix dbUrl

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const helmet = require("helmet");
 const userRoutes = require("./routes/users");
 const campgroundsRoutes = require("./routes/campgrounds");
 const reviewsRoutes = require("./routes/reviews");
-const dbUrl = process.env.DB_URL || "mongodb://127.0.0.1:27017/yelp-camp";
+const dbUrl = "mongodb://127.0.0.1:27017/yelp-camp";
 const MongoStore = require ("connect-mongo");
 
 mongoose.connect(dbUrl);
